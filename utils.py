548af2328b0cacf9c53246f5d17fcdd03f22d7b4
Carolina Amorim <carolinaoa1@al.insper.edu.com>
2025-09-08 13:51:56 -0300

    início de algumas funções no utils

--- utils.py
+++ utils.py
@@ -1,1 +1,40 @@
 import sqlite3 as sql
+
+def imoveis():
+    data = sql.connect('imoveis.sql')
+    cur = data.cursor()
+    cur.execute(
+    """SELECT * FROM imoveis"""
+    )
+    todos_imoveis = cur.fetchall()
+    return todos_imoveis
+
+def especifico(imovel_id):
+    data = sql.connect('imoveis.sql')
+    cur = data.cursor()
+    cur.execute(
+    'SELECT * FROM imoveis WHERE id = ? ',
+    (imovel_id, ))
+    imovel_especifico = cur.fetchall()
+    return imovel_especifico
+
+def add(imovel_id, logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisição):
+    data = sql.connect('banco.db')
+    cur = data.cursor()
+    cur.execute(
+    'INSERT INTO imoveis (imovel_id, logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisição) VALUES (?,?,?,?,?,?,?,?,?)', (imovel_id, logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisição)
+    )
+    novo_imovel = cur.fetchall()
+    data.commit()
+    data.close()
+    
+    return novo_imovel
+
+# def update():
+
+# def remove():
+
+# def type():
+
+# def city():
+
